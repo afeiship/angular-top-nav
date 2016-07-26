a4cca3d5357cfcbbacf68471952ee41fc91ec093
feizheng <1290657123@qq.com>
2016-07-27 00:47:03 +0800

    optimize

--- src/angular/directive/top-nav.js
+++ src/angular/directive/top-nav.js
@@ -12,12 +12,33 @@
           itemClick:'&'
         },
         template: '<div class="nx-widget-top-nav {{cssClass}}">' +
-        '<div class="nx-item" ng-repeat="item in items">'+
-        '<a sref="item.action" ng-click="itemClick()(item)">{{item.text}}</a>'+
+        '<div class="nx-item" ng-repeat="item in items" data-active="{{item.active}}">'+
+        '<a sref="item.action" ng-click="itemClickInner(item)">{{item.text}}</a>'+
         '</div>'+
         '<b class="bar" ng-style="{width:100/items.length+\'%\',left:100*activeIndex/items.length+\'%\'}"></b>'+
-        '</div>'
+        '</div>',
+        link:linkFn
       };
+
+
+      function linkFn(scope,el,attrs) {
+        var _activeIndex=scope.activeIndex || 0;
+        scope.items[_activeIndex].active=true;
+        scope.itemClickInner=function(inItem) {
+
+          scope.items.forEach(function(item,index){
+            if(angular.equals(item,inItem)){
+                scope.activeIndex=index;
+            }
+            item.active=false;
+          });
+          inItem.active=true;
+
+
+          //delegate:
+          scope.itemClick()(inItem);
+        }
+      }
 
     }]);
 
